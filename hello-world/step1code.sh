f7743518eb02cec50a0133d392940851e83e44d3
Christie Molloy <cmolloy@redhat.com>
2019-05-31 15:15:05 -0400

    Create step1code.sh

---
+++ hello-world/step1code.sh
@@ -0,0 +1,4 @@
+<script src="//katacoda.com/embed.js"></script>
+<div id="my-element-1" data-katacoda-env="node" data-katacoda-layout="editor" style="border: 2px solid #ccc;">
+  console.log("Hello World!!");
+</div>
