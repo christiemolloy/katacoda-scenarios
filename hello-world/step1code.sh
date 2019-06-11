92d82e3ca002a29be487664b39eb47ab3f92a6ae
Christie Molloy <cmolloy@redhat.com>
2019-06-11 15:38:01 -0400

    Delete step1code.sh

--- hello-world/step1code.sh
+++
@@ -1,4 +0,0 @@
-<script src="//katacoda.com/embed.js"></script>
-<div id="my-element-1" data-katacoda-env="node" data-katacoda-layout="editor" style="border: 2px solid #ccc;">
-  console.log("Hello World!!");
-</div>
